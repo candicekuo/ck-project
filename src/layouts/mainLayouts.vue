7b4a95836a45631b2a84da7e2cbca0b0d061cf07
Candice Kuo <killua7812@gmail.com>
2021-12-21 16:45:36 +0800

    fix: layout

--- src/layouts/mainLayouts.vue
+++ src/layouts/mainLayouts.vue
@@ -1,26 +1,23 @@
 <template>
-    <div>
- 
-        <header></header>
-
-        <footer></footer>
-        <!-- 在這分割header footer -->
-        <p>test</p>
-    </div>
+  <div>
+    <header></header>
+    <footer></footer>
+    <!-- 在這分割header footer -->
+    <p>{{ test }}</p>
+  </div>
 </template>
 
 <script>
-
 // import selectMixins from 'mixins/select';
 // import CustomDialog from 'components/CustomDialog';
 
 export default {
   name: 'mainLayouts',
-  components: {
-  },
+  components: {},
   data() {
     return {
-
+      test: 'hello',
     };
   },
-}
+};
+</script>
