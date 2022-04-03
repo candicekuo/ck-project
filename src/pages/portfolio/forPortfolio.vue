eeaebc4daf79d27d88593dcc1f586f7f29b80dee
candice <killua7812@gmail.com>
2022-04-04 01:23:01 +0800

    🚧 img

--- src/pages/portfolio/forPortfolio.vue
+++ src/pages/portfolio/forPortfolio.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="col-xs-12 col-sm-12 col-md-7 col-lg-8 col-xl-9">sdfgvhbnm,r</div>
+  <div class="col-xs-12 col-sm-12 col-md-7 col-lg-8 col-xl-9">
+    <div v-for="(data, index) in ehr" :key="index">
+      bfghjkl
+      <img :src="require(data)" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,7 +14,14 @@ export default {
   data() {
     return {
       test: 'hello',
+      // imgData: {
+      ehr: [
 
+        '@/assets/img/1hr-1.png',
+        '@/assets/img/1hr-2.png'
+        // imgPath: require('@/assets/img/1hr-1.png'),
+      ]
+      // },
     };
   },
   methods: {},
