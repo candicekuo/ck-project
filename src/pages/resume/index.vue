ce7e3be316b4e9011bcab6255d9a15047937b1ff
candice <killua7812@gmail.com>
2022-04-11 23:01:48 +0800

    ✨ resume UI

--- src/pages/resume/index.vue
+++ src/pages/resume/index.vue
@@ -1,5 +1,137 @@
 <template>
-  <div>Resume building...</div>
+  <div class="container">
+    <div class="row setDetail upAnimation">
+      <div class="col-xl-6 col-lg-6 col-md-12 leftLine">
+        <div class="card personal rotates">
+          <img src="@/assets/img/pic2.png" alt="" />
+          <div class="marginT">
+            <p>Candice <small>( Taiwan )</small></p>
+            <p>
+              女性 <span class="text-grey">｜</span> 33歲 <span class="text-grey">｜</span> Ｏ型
+            </p>
+            <p class="marginT">聯絡地址 : 台北市信義區</p>
+            <p>身高體重 : 165cm <span class="text-grey">｜</span> 50kg</p>
+          </div>
+        </div>
+        <div class="card lineBox">
+          <p class="title">學歷</p>
+          <div class="educationBox">
+            <p>中國科技大學 (畢業) 2007/09 - 2011/06</p>
+            <p class="detail">視覺傳達設計系</p>
+            <hr />
+            <p>大安高工 (畢業) 2004/09 - 2007/06</p>
+            <p class="detail">圖文傳播科</p>
+          </div>
+        </div>
+        <div class="itemR">
+          <p>2020/11 - 在職中</p>
+        </div>
+        <div class="card lineBox">
+          <p class="title YColor">全職</p>
+          <p class="jobTitle">前端工程師</p>
+          <p class="text-blue text-big">酷堤康科技股份有限公司</p>
+          <p class="text-blue">
+            台北市中山區 <span class="text-grey">｜</span> 電腦軟體服務業
+            <small>( 30 - 100人 )</small>
+          </p>
+          <div class="jobInfo">
+            <p>工作說明</p>
+            <ol>
+              <li>使用 Vue + Quasar 開發稅務系統、後台系統</li>
+              <li>網站 UI 切版、特效製作</li>
+              <li>網站功能開發</li>
+              <li>與後端工程師配合串接API</li>
+              <li>後台系統優化UIUX</li>
+            </ol>
+          </div>
+        </div>
+        <div class="itemR">
+          <p>2016/11 - 2020/05</p>
+        </div>
+        <div class="card lineBox">
+          <p class="title YColor">全職</p>
+          <p class="jobTitle">網頁設計師 <small>( 3年6個月 )</small></p>
+          <p class="text-blue text-big">全球華人股份有限公司</p>
+          <p class="text-blue">
+            台北市松山區 <span class="text-grey">｜</span> 人力銀行相關 <small>( 500人以上 )</small>
+          </p>
+          <div class="jobInfo">
+            <p>工作說明</p>
+            <ol>
+              <li>系統 UIUX 優化</li>
+              <li>網站介面改版規劃設計</li>
+              <li>RWD 響應式網站製作</li>
+              <li>活動頁面視覺規劃、頁面互動設計</li>
+              <li>測驗頁視覺規劃、特效製作</li>
+              <li>網頁編輯互動、功能開發製作</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-6 col-lg-6 col-md-12">
+        <div class="card">
+          <p>Frontend</p>
+          <p class="detail">
+            熟悉 HTML、 CSS SCSS、Tailwind css、Javascript ES6、jQuery
+            、Bootstrap3,4、Vue.js、Vuex、Router、Quasar、Git，可獨立切版、優化 UIUX、完成RWD網頁
+          </p>
+          <hr />
+          <p>平面設計</p>
+          <p class="detail">
+            熟悉 Illustrator、Photoshop、Figma，相關軟體操作，擅長影像處理＆版面視覺
+          </p>
+        </div>
+        <div class="itemC">
+          <p><i class="bi bi-clipboard2-minus"></i> 求職條件</p>
+        </div>
+        <div class="card">
+          <div>
+            <p>應徵職務</p>
+            <p class="detail">前端工程師</p>
+          </div>
+          <div>
+            <p>期望薪資</p>
+            <p class="detail">面議</p>
+          </div>
+          <div>
+            <p>可上班日</p>
+            <p class="detail">錄取30天後可上班</p>
+          </div>
+          <div>
+            <p>上班地點</p>
+            <p class="detail">
+              台北市松山區、新北市板橋區、台北市信義區、台北市大安區、台北市中山區
+            </p>
+          </div>
+          <div>
+            <p>上班時段</p>
+            <p class="detail">日班</p>
+          </div>
+          <div>
+            <p>休假時段</p>
+            <p class="detail">週休二日</p>
+          </div>
+        </div>
+        <div class="itemC">
+          <p><i class="bi bi-file-earmark-person"></i> 自 傳</p>
+        </div>
+        <div class="card autobiography">
+          <p>
+            在這段不長也不短的工作經歷中，覺得工作中最難的就是如何溝通與妥協，大家從不同的角度看同一件事情各持己見，有自己覺得該堅持的理由，要如何從中取捨，解決問題，是最近學到覺得最重要的一件事。
+          </p>
+          <p>
+            一直以來都做著設計相關的工作，設計源頭是解決問題，能夠運用設計幫忙解決問題進而得到成就感，是我在設計這個位子上的熱情來源。
+          </p>
+          <p>
+            參予公司內各種專案，從視覺設計到網頁系統操作流程規劃，工作中學到了很多寫網頁的技巧，懂得架構的重要性與資料是如何處理，和後端工程師配合等等的事情。
+          </p>
+          <p>
+            人生的旅途很長，一路上也經歷過許多波折，感謝在路途中給予協助、鼓勵，並讓我成長的人們。我非常樂意接受挑戰與新的學習，希望有機會與貴公司一同面對挑戰、學習成長，謝謝您！
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,23 +146,230 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-div {
+p {
+  line-height: 1.875rem;
+}
+.text-big {
+  font-size: 1.125rem;
+}
+.setDetail {
+  margin: 60px 0;
+  padding: 60px 0;
+  background: $bluelight;
+  box-shadow: 2px 2px 6px $grey9;
+}
+.leftLine {
+  border-right: 1px solid $grey9;
+
+  &::before,
+  &::after {
+    content: '';
+    width: 10px;
+    height: 10px;
+    display: inline-block;
+    background: $darkBlue;
+    border-radius: 25px;
+    position: absolute;
+    right: -5px;
+  }
+  &::before {
+    top: 0;
+  }
+  &::after {
+    bottom: 0;
+  }
+}
+.lineBox {
+  &::before,
+  &::after {
+    content: '';
+    width: 10px;
+    height: 10px;
+    display: inline-block;
+    background: $darkBlue;
+    border-radius: 25px;
+    position: absolute;
+    right: -37px;
+  }
+  &::before {
+    top: calc(50% - 5px);
+  }
+  &::after {
+    top: 50%;
+    height: 1px;
+    width: 37px;
+  }
+}
+.marginT {
+  margin-top: 10px;
+}
+.personal {
+  display: flex;
+  flex-flow: row nowrap;
+  img {
+    width: 120px;
+    height: auto;
+    border: 5px solid white;
+    box-shadow: 1px 1px 4px #ccc;
+    margin-right: 25px;
+  }
+}
+.card {
+  border: 1px solid $ddd;
+  border-radius: 6px;
+  padding: 15px;
+  margin: 0 15px 25px;
+  background: $white;
+  position: relative;
+
+  ol {
+    line-height: 1.875rem;
+    li {
+      list-style-type: decimal;
+    }
+  }
+  .jobTitle {
+    margin: -4px 0 10px 60px;
+  }
+  .jobInfo {
+    margin-top: 6px;
+    ol {
+      border-left: 2px solid $ccc;
+      margin-left: 6px;
+      padding-left: 32px;
+    }
+  }
+  .title {
+    display: detail-block;
+    background: #4f9ebc;
+    color: white;
+    text-align: center;
+    padding: 0px 18px;
+    border-top-right-radius: 25px;
+    border-bottom-right-radius: 25px;
+    letter-spacing: 3px;
+    position: absolute;
+    top: 10px;
+    left: -10px;
+  }
+  .educationBox {
+    p {
+      &:first-child {
+        margin-top: 30px;
+      }
+    }
+  }
+
+  .YColor {
+    background: #f6cb45;
+    color: $grey3;
+  }
+}
+.detail {
+  border-left: 2px solid $ccc;
+  padding-left: 12px;
+  margin: 6px;
+  color: $grey6;
+}
+.itemR {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 15px;
+  margin-bottom: 4px;
+}
+.itemC {
   display: flex;
   justify-content: center;
-  animation-name: flash;
-  animation-duration: 1s;
-  animation-iteration-count: infinite;
+  margin-right: 15px;
+  margin-bottom: 4px;
 }
 
+.autobiography {
+  p {
+    margin: 0 8px 16px;
+    color: $grey6;
+    &:first-child {
+      margin-top: 16px;
+    }
+  }
+}
+
+// Extra small devices (portrait phones, less than 576px) xs
+@media (max-width: 575.98px) {
+  .lineBox,
+  .leftLine {
+    border: 0;
+
+    &::after,
+    &::before {
+      display: none;
+    }
+  }
+  .personal {
+    flex-flow: row wrap;
+    justify-content: center;
+  }
+}
+
+// Small devices (landscape phones, 576px and up) sm
+@media (min-width: 576px) and (max-width: 767.98px) {
+  .lineBox,
+  .leftLine {
+    border: 0;
+
+    &::after,
+    &::before {
+      display: none;
+    }
+  }
+}
+
+// Medium devices (tablets, 768px and up) md
+@media (min-width: 768px) and (max-width: 991.98px) {
+  .lineBox,
+  .leftLine {
+    border: 0;
+
+    &::after,
+    &::before {
+      display: none;
+    }
+  }
+}
+
+// Large devices (desktops, 992px and up) lg
+@media (min-width: 992px) and (max-width: 1199.98px) {
+}
+
+// Extra large devices (large desktops, 1200px and up) xl
+@media (min-width: 1200px) {
+}
+
+.upAnimation {
+  animation-name: flash;
+  animation-duration: 1s;
+}
 @keyframes flash {
   0% {
     opacity: 0;
-  }
-  50% {
-    opacity: 1;
+    margin-top: 200px;
   }
   100% {
-    opacity: 0;
+    opacity: 1;
+    margin-top: 60px;
+  }
+}
+.rotates {
+  animation-name: rotates;
+  animation-duration: 1s;
+}
+
+@keyframes rotates {
+  0% {
+    transform: rotateX(180deg);
+  }
+  100% {
+    transform: rotateX(0deg);
   }
 }
 </style>
